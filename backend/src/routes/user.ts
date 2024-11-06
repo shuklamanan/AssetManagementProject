9087ef452d2ef141243988dd6a9be55b38c6e6c8
shuklamanan <mananshukla1512@gmail.com>
2024-11-06 19:15:17 +0530

    Feat create API for Creation of user, read data of all user and profile of user

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,15 +1,19 @@
 import express, {Router} from 'express';
-
 import {
     createUser,
     loginUser,
-    getRoles
+    getRoles,
+    getAllUsers,
+    getProfileDetails
 } from '../controllers/user.ts';
+import {verifyJwt} from "../middleware/verifyJWT.ts";
+import {verifyRole} from "../middleware/verifyRole.ts";
 
-import {verifyJwt} from "../middleware/varifyJWT";
 export const userRoutes:Router = express.Router();
 
 userRoutes.post('/login',loginUser)
 userRoutes.post('/signup',createUser)
 userRoutes.get('/roles',verifyJwt,getRoles)
-
+userRoutes.get('/profile',verifyJwt,getProfileDetails);
+userRoutes.get('/',verifyJwt,verifyRole(['Admin']),getAllUsers)
+userRoutes.post('/',verifyJwt,verifyRole(['Admin']),createUser)

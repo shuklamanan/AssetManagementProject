1ab02666d1035f5806fe29552a5f82b28d998494
shuklamanan <mananshukla1512@gmail.com>
2024-11-06 19:17:33 +0530

    Feat assets.ts file

---
+++ backend/src/controllers/asset.ts
@@ -0,0 +1,152 @@
+import client from "../../postgresConfig.ts"
+import {Request, Response} from "express";
+import {
+    IAsset, ICreateAssetQueryBody, IDecideHolderOfAsset,
+    IMergeDetailsOfAssetAndUser,
+    IUserAndAsset,
+} from "../interfaces/requestWithUser.ts";
+
+
+export const createAssets = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {
+            name,
+            assetType,
+            config,
+            userId
+        } = req.body as ICreateAssetQueryBody;
+        if (!(name && assetType && config)) {
+            res.status(400).json({message: "some required fields are missing in req"})
+            return;
+        }
+        const response: IAsset = await client.query("insert into assets(name, asset_type, config,user_id) values ($1,$2,$3,$4) RETURNING *", [name, assetType, JSON.stringify(config), userId ?? null])
+        if (userId) {
+            await client.query("INSERT INTO asset_history (asset_id,user_id,assigned_by,assigned_at) VALUES ($1,$2,$3,now())", [response?.rows[0].id, req.body.user_id, req.body.user.id]);
+        }
+        res.status(201).json({message: "asset created successfully"});
+        return;
+    } catch (error: any) {
+        res.status(500).json({message: error?.message});
+        return;
+    }
+}
+
+export const getAllAssets = async (req: Request, res: Response): Promise<void> => {
+    try {
+        let response : IUserAndAsset;
+        if (req.body.user.role.includes("Admin")) {
+            response = await client.query(`SELECT 
+                                                  a.id,
+                                                  a.name,
+                                                  a.asset_type,
+                                                  a.user_id,
+                                                  a.config,
+                                                  u.username,
+                                                  u.first_name,
+                                                  u.last_name,
+                                                  u.email,
+                                                  u.phone_number
+                                           FROM assets a
+                                                    LEFT JOIN users u ON u.id = a.user_id
+                                           WHERE a.archived_at IS NULL
+                                             AND u.archived_at IS NULL
+            `);
+        } else {
+            response = await client.query(`SELECT a.id,
+                                                  a.name,
+                                                  a.asset_type,
+                                                  a.user_id,
+                                                  a.config,
+                                                  u.username,
+                                                  u.first_name,
+                                                  u.last_name,
+                                                  u.email,
+                                                  u.phone_number
+                                           FROM assets a
+                                                    LEFT JOIN users u ON u.id = a.user_id
+                                           WHERE u.username = $1
+                                             AND a.archived_at IS NULL
+                                             AND u.archived_at IS NULL
+            `, [req.body.user.username]);
+        }
+        const allAssets : IMergeDetailsOfAssetAndUser[] = response?.rows;
+        res.status(200).json(allAssets ?? []);
+        return;
+    } catch (error: any) {
+        res.status(500).json({message: error?.message});
+        return;
+    }
+}
+
+export const updateAsset = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const id: string = req.params.id;
+        const {
+            name,
+            assetType,
+            config,
+            userId,
+        } = req.body as ICreateAssetQueryBody;
+        await client.query("UPDATE assets SET name = $1 , asset_type = $2, config = $3, user_id=$4 WHERE id=$5", [name, assetType, JSON.stringify(config), userId ?? null, id])
+        res.status(200).json({message: "asset updated successfully"});
+        return;
+    } catch (error: any) {
+        res.status(500).json({message: error?.message});
+        return;
+    }
+}
+
+export const assetAssign = async (req: Request, res: Response): Promise<void> => {
+    try {
+        if (!req.body.userId || !req.body.assetId) {
+            res.status(400).send({error: "Please enter all required fields"});
+            return;
+        }
+        const response: IDecideHolderOfAsset = await client.query("SELECT 1 AS asset_holder_or_not FROM assets WHERE id=$1 AND user_id IS NOT NULL", [req.body.assetId])
+        console.log(req.body);
+        console.log(response?.rows)
+        if (!response?.rows[0]?.asset_holder_or_not) {
+            await client.query("UPDATE assets SET user_id = $1 WHERE id = $2", [req.body.userId, req.body.assetId]);
+            await client.query("INSERT INTO asset_history (asset_id,user_id,assigned_by,assigned_at) VALUES ($1,$2,$3,now())", [req.body.assetId, req.body.userId, req.body.user.id]);
+            res.status(201).json({"message": "successfully assigning user"});
+            return;
+        }
+        res.status(201).json({"message": "Asset has been already assigned to someone"});
+        return;
+    } catch (error: any) {
+        res.status(500).json({message: error?.message});
+        return;
+    }
+}
+
+export const assetUnassign = async (req: Request, res: Response): Promise<void> => {
+    try {
+        if (!req.params.id) {
+            res.status(400).send({error: "Please enter all required fields"});
+            return;
+        }
+        const response: IAsset = await client.query("SELECT * FROM assets WHERE user_id IS NOT NULL AND id=$1", [req.params.id])
+        if (response?.rows.length == 0) {
+            res.status(400).json({message: "Asset is already unassigned"});
+            return;
+        }
+        console.log(response.rows[0]);
+        await client.query("UPDATE asset_history SET unassigned_at = now(),assigned_by = $3 WHERE asset_id=$1 AND user_id=$2 AND unassigned_at IS NULL RETURNING *", [req.params.id, response.rows[0].user_id, req.body.user.id]);
+        await client.query("UPDATE assets SET user_id = null WHERE id=$1", [req.params.id]);
+        res.status(201).json({"message": "successfully unassigning asset"});
+        return;
+    } catch (error: any) {
+        res.status(500).json({message: error?.message});
+        return;
+    }
+}
+export const getAssetHistory = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const response = await client.query("SELECT ah.user_id, u.username,a.id as asset_id,a.name as asset_name,ah.assigned_at,ah.unassigned_at FROM asset_history ah LEFT JOIN users u  ON u.id = ah.user_id JOIN assets a ON ah.asset_id = a.id ")
+        res.status(200).json(response?.rows);
+        return;
+    } catch (error: any) {
+        res.status(500).json({message: error?.message});
+        return;
+    }
+}

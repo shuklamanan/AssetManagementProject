02327d05ac63c3bb3e8cc863ae9c438195352241
shuklamanan <mananshukla1512@gmail.com>
2024-11-06 19:18:33 +0530

    Feat Add get data of user Refactor user file

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,16 +1,21 @@
 import client from "../../postgresConfig.ts"
 import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken';
+import jwt, {JwtPayload} from 'jsonwebtoken';
 import dotenv from "dotenv";
-import {Response, Request} from "express";
-import {requestWithUser} from "../interfaces/requestWithUser.ts";
+import {Request, Response} from "express";
+import {
+    ICreateUserQueryBody,
+    ICreateUserRequestBody,
+    ILoginUserRequestBody,
+    IUser
+} from "../interfaces/requestWithUser.ts";
 
 dotenv.config()
 
 async function hashPassword(password: string): Promise<string> {
-    const saltRounds = 10;
-    return await new Promise((resolve, reject) => {
-        bcrypt.hash(password, saltRounds, function (err, hash) {
+    const saltRounds : number = 10;
+    return await new Promise((resolve, reject) : void => {
+        bcrypt.hash(password, saltRounds, function (err :Error | undefined, hash : string) {
             if (err) reject(err)
             resolve(hash)
         });
@@ -18,37 +23,31 @@ async function hashPassword(password: string): Promise<string> {
 }
 
 
-const generateToken = (payload: { username: string }) => {
-    const secretKey = process.env.ACCESS_TOKEN_SECRET??""; // Replace with your own secret key
+const generateToken = (payload: JwtPayload) => {
+    const secretKey: string = process.env.ACCESS_TOKEN_SECRET ?? ""; // Replace with your own secret key
     const options = {
         expiresIn: '24h',
     };
     return jwt.sign(payload, secretKey, options);
 };
 
-function isValidPassword(password: string) {
-    const regex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,20}$/;
+function isValidPassword(password: string) : boolean {
+    const regex : RegExp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,20}$/;
     return regex.test(password);
 }
 
-interface createUserRequestBody {
-    username: string,
-    firstName: string,
-    lastName: string,
-    email: string,
-    password: string,
-    phoneNo: number,
-    department: string,
-    dateOfBirth: string
-}
-interface loginUserRequestBody {
-    username: string,
-    password: string
-}
-
-export const createUser = async (req:Request, res:Response):Promise<void> => {
+export const createUser = async (req: Request, res: Response): Promise<void> => {
     try {
-        let {username, firstName, lastName, email, password, phoneNo, department, dateOfBirth} = req.body as createUserRequestBody;
+        let {
+            username,
+            firstName,
+            lastName,
+            email,
+            password,
+            phoneNumber,
+            department,
+            dateOfBirth
+        } = req.body as ICreateUserQueryBody;
         if (!isValidPassword(password)) {
             res.status(400).json({
                 message: '`Password should contain:-\n' +
@@ -61,54 +60,83 @@ export const createUser = async (req:Request, res:Response):Promise<void> => {
             });
             return
         }
-        if (!(username && email && password && firstName && lastName && phoneNo)) {
+        if (!(username && email && password && firstName && lastName && phoneNumber)) {
             res.status(400).json({message: "some required fields are missing in req"})
             return;
         }
-        await client.query("insert into users(username, first_name, last_name, role, email, password, phone_number, department, date_of_birth) values ($1,$2,$3,Array ['Employee']::role[],$4,$5,$6,$7,$8)", [username, firstName, lastName, email, await hashPassword(password), phoneNo, department ?? null, dateOfBirth ?? null])
+        await client.query("insert into users(username, first_name, last_name, role, email, password, phone_number, department, date_of_birth) values ($1,$2,$3,Array ['Employee']::role[],$4,$5,$6,$7,$8)", [username, firstName, lastName, email, await hashPassword(password), phoneNumber, department ?? null, dateOfBirth ?? null])
         res.status(201).json({message: "user created successfully"});
         return;
-    } catch (error:any) {
+    } catch (error: any) {
         res.status(500).json({message: error?.message});
         return;
     }
 };
-export const loginUser = async (req:Request, res:Response):Promise<void> => {
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
     try {
-        let {username, password} = req.body as loginUserRequestBody
+        let {username, password} = req.body as ILoginUserRequestBody
         if (!(username && password)) {
-             res.status(400).json({message: "some fields are missing in req"})
+            res.status(400).json({message: "some fields are missing in req"})
             return
         }
-        let response = await client.query("select * from users where username=$1", [username]);
+        let response : IUser = await client.query("select * from users where username=$1", [username]);
         if (response.rows.length === 0) {
-             res.status(404).json({message: "user not found"})
+            res.status(404).json({message: "user not found"})
             return
         }
-        let user = response.rows[0]
+        let user: ICreateUserRequestBody = response.rows[0]
         if (!bcrypt.compareSync(password, user.password)) {
-             res.status(401).json({message: "user auth failed incorrect username or password"})
+            res.status(401).json({message: "user auth failed incorrect username or password"})
             return
         }
-        let payload = {
-            username: username,
-        }
-        let token = generateToken(payload)
+        let payload : JwtPayload = { id: user.id }
+        let token: string = generateToken(payload)
         res.status(200).json({token: token});
         return
-    } catch (error:any) {
+    } catch (error: any) {
         res.status(500).json({message: error?.message});
         return
     }
 };
-export const getRoles = async (req:Request, res:Response):Promise<void> => {
+export const getRoles = async (req: Request, res: Response): Promise<void> => {
     try {
         res.status(200).json({roles: req.body.user.role.substring(1, req.body.user.role.length - 1).split(",")});
         return
-    } catch (error:any) {
+    } catch (error: any) {
         res.status(500).json({message: error?.message});
         return
     }
 };
 
+export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const response: IUser = await client.query("select username,first_name,last_name,role,email,phone_number,department,date_of_birth from users WHERE archived_at IS NULL ORDER BY role")
+        const allUsers : ICreateUserRequestBody[] = response?.rows;
+        res.status(200).json(allUsers ?? []);
+        return;
+    } catch (error: any) {
+        res.status(500).json({message: error?.message});
+        return;
+    }
+}
 
+export const getProfileDetails = async (req: Request, res: Response): Promise<void> => {
+    try {
+        console.log(req.body.user);
+        res.status(200).json({
+            "username": req.body.user.username,
+            "firstName": req.body.user.first_name,
+            "lastName": req.body.user.last_name,
+            "role": req.body.user.role,
+            "email": req.body.user.email,
+            "phone_number": req.body.user.phone_number,
+            "department": req.body.user.department,
+            "dateOfBirth": req.body.user.date_of_birth,
+            "joiningDate": req.body.user.created_at,
+        });
+        return;
+    } catch (error: any) {
+        res.status(500).json({message: error?.message});
+        return;
+    }
+}

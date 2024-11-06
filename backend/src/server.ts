ff0ff5d9ca8d35e9c49e5377fd45d1d5dc6416d4
shuklamanan <mananshukla1512@gmail.com>
2024-11-06 19:13:08 +0530

    Feat Asset routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'
 import {userRoutes} from './routes/user.ts';
-
+import {assetRoutes} from './routes/asset.ts';
 
 dotenv.config()
 const app = express();
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/assets',assetRoutes);
 const port = process.env.BACKEND_PORT || 5000
 app.listen(port, () =>
     console.log(`Server is live @ `+port)

7348d0a1bedf34e600e626cde9496f2e77f2e176
shuklamanan <mananshukla1512@gmail.com>
2024-11-07 14:33:23 +0530

    Feat add delete modal and open modal for assets

--- frontend/src/ts/main.ts
+++ frontend/src/ts/main.ts
@@ -1,16 +1,16 @@
 import fetchUserRoles from '../functions/fetchUserRoles.ts'
-
+const commonApi:string = "http://localhost:5001";
 if (localStorage.getItem("token") === null || localStorage.getItem("token") === undefined) {
     window.location.href = "/src/html/login.html";
 }
 
 function displayContentBasedOnRoles(roles: string[]): void {
     if (!roles.includes('Admin')) {
-        document.getElementById("user-nav").style.display = "none"
+        document.getElementById("user-nav")!.style.display = "none"
     }
 }
 
-const roles = await fetchUserRoles();
+const roles : string[] = await fetchUserRoles();
 displayContentBasedOnRoles(roles);
 fetchAssets()
 interface Asset {
@@ -21,70 +21,104 @@ interface Asset {
     config: Record<string, string>;
 }
 let assets: Asset[] = [];
-async function fetchAssets() {
-    const response = await fetch('http://localhost:5001/assets', {
+async function fetchAssets() : Promise<void> {
+    const getAssetApi : string = commonApi + "/assets/";
+    const response : Response = await fetch(getAssetApi, {
         headers: {
             'Authorization': `${localStorage.getItem('token')}`,
             'Content-Type': 'application/json'
         }
     });
     assets = await response.json();
+    console.log(assets);
     displayAssets(assets);
 }
 
-function displayAssets(assets: Asset[]) {
-    const tbody = document.getElementById('assetTableBody') as HTMLElement;
+function displayAssets(assets: Asset[]) : void {
+    const tbody : HTMLElement = document.getElementById('assetTableBody') as HTMLElement;
 
-    assets.forEach(asset => {
-        const row = document.createElement('tr');
+    assets.forEach(asset  => {
+        const row : HTMLTableRowElement = document.createElement('tr');
 
-        const idCell = document.createElement('td');
+        const idCell : HTMLTableCellElement = document.createElement('td');
         idCell.textContent = asset.id.toString();
         row.appendChild(idCell);
 
-        const nameCell = document.createElement('td');
+        const nameCell: HTMLTableCellElement = document.createElement('td');
         nameCell.textContent = asset.name;
         row.appendChild(nameCell);
 
-        const typeCell = document.createElement('td');
+        const typeCell : HTMLTableCellElement = document.createElement('td');
         typeCell.textContent = asset.asset_type;
         row.appendChild(typeCell);
 
-        const ownerCell = document.createElement('td');
+        const ownerCell : HTMLTableCellElement = document.createElement('td');
         ownerCell.textContent = asset.username || 'Unassigned';
         row.appendChild(ownerCell);
 
-        const openButtonCell = document.createElement('td');
-        const openButton = document.createElement('button');
+        const openButtonCell : HTMLTableCellElement = document.createElement('td');
+        const openButton : HTMLButtonElement = document.createElement('button');
+        openButton.type = 'button';
         openButton.className = 'btn btn-primary';
         openButton.textContent = 'Open';
-        openButton.setAttribute('data-bs-toggle', 'modal');
-        openButton.setAttribute('data-bs-target', '#assetModal');
-        openButton.onclick = () => openAsset(asset.id);
+        openButton.setAttribute('data-toggle', 'modal');
+        openButton.setAttribute('data-target', '#assetModal');
+        openButton.onclick = () => openAsset(asset);
         openButtonCell.appendChild(openButton);
         row.appendChild(openButtonCell);
 
-        const deleteButtonCell = document.createElement('td');
-        const deleteButton = document.createElement('button');
+        const deleteButtonCell: HTMLTableCellElement = document.createElement('td');
+        const deleteButton : HTMLButtonElement = document.createElement('button');
         deleteButton.className = 'btn btn-danger';
+        deleteButton.type = 'button';
+        deleteButton.setAttribute('data-toggle', 'modal');
+        deleteButton.setAttribute('data-target', '#deleteModal');
         deleteButton.textContent = 'Delete';
-        deleteButton.onclick = () => deleteAsset(asset.id);
+        const confirmDeleteAssetBtn : HTMLButtonElement = <HTMLButtonElement>document.getElementById('confirmDeleteAssetBtn')!;
+        confirmDeleteAssetBtn.onclick = () => deleteAsset(asset.id);
         deleteButtonCell.appendChild(deleteButton);
         row.appendChild(deleteButtonCell);
-
         tbody.appendChild(row);
         console.log(row)
     });
 
 }
 
-function deleteAsset(assetId: number) {
-    document.getElementById(`${assetId}`)?.remove()
+async function deleteAsset(id:number) : Promise<void> {
+    const deleteAssetApi : string = commonApi + `/assets/${id}`;
+    const response : Response = await fetch(deleteAssetApi, {
+        headers: {
+            'Authorization': `${localStorage.getItem('token')}`,
+            'Content-Type': 'application/json'
+        },
+        method: "DELETE"
+    });
+    assets = await response.json();
+    console.log(assets);
+    window.location.reload();
 }
 
+function openAsset(asset : Asset) : void {
+    const modalAssetId:HTMLElement = document.getElementById("modalAssetId")!;
+    const modalAssetName:HTMLElement = document.getElementById("modalAssetName")!;
+    const modalAssetType:HTMLElement = document.getElementById("modalAssetType")!;
+    const modalAssetOwner:HTMLElement = document.getElementById("modalAssetOwner")!;
+    const tableBody : HTMLElement = document.getElementById('configTableBody')!;
+    modalAssetId.textContent = asset.id.toString();
+    modalAssetName.textContent = asset.name;
+    modalAssetType.textContent = asset.asset_type;
+    modalAssetOwner.textContent = asset.username || 'Unassigned';
+    console.log(asset.config);
+    for(let key in asset.config){
+        const row : HTMLTableRowElement = document.createElement('tr');
 
+        const objectKeyCell : HTMLTableCellElement = document.createElement('td');
+        objectKeyCell.textContent = key;
+        row.appendChild(objectKeyCell);
 
-
-
-
-
+        const objectValueCell: HTMLTableCellElement = document.createElement('td');
+        objectValueCell.textContent = asset.config[key];
+        row.appendChild(objectValueCell);
+        tableBody.appendChild(row);
+    }
+}

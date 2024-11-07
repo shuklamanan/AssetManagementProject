4984f6fac6df35c8ecc7fabc1b85b0c9837a1b46
shuklamanan <mananshukla1512@gmail.com>
2024-11-07 14:33:56 +0530

    Refactor profile page

--- frontend/src/ts/profile.ts
+++ frontend/src/ts/profile.ts
@@ -6,15 +6,15 @@ if (localStorage.getItem("token") === null || localStorage.getItem("token") === 
 
 async function displayContentBasedOnRoles(roles: string[]): Promise<void> {
     if (!roles.includes('Admin')) {
-        document.getElementById("user-nav").style.display = "none";
+        document.getElementById("user-nav")!.style.display = "none";
     }
 }
 
 const roles = await fetchUserRoles();
 displayContentBasedOnRoles(roles);
 
-async function fetchUserProfile() {
-    const response = await fetch('http://localhost:5001/profile', {
+async function fetchUserProfile() : Promise<void> {
+    const response : Response = await fetch('http://localhost:5001/users/profile', {
         headers: {
             'Authorization': `${localStorage.getItem('token')}`,
             'Content-Type': 'application/json'
@@ -23,12 +23,12 @@ async function fetchUserProfile() {
     const userData = await response.json();
 
     (document.getElementById('username') as HTMLElement).textContent = userData.username || "N/A";
-    (document.getElementById('firstName') as HTMLElement).textContent = userData.first_name || "N/A";
-    (document.getElementById('lastName') as HTMLElement).textContent = userData.last_name || "N/A";
+    (document.getElementById('fullName') as HTMLElement).textContent = userData.first_name + " " + userData.last_name || "N/A";
     (document.getElementById('email') as HTMLElement).textContent = userData.email || "N/A";
-    (document.getElementById('department') as HTMLElement).textContent = userData.department || "N/A";
+    // (document.getElementById('department') as HTMLElement).textContent = userData.department || "N/A";
     (document.getElementById('phoneNumber') as HTMLElement).textContent = userData.phone_number || "N/A";
-    (document.getElementById('dob') as HTMLElement).textContent = userData.date_of_birth || "N/A";
+    (document.getElementById('dob') as HTMLElement).textContent = userData.date_of_birth.substring(0,10) || "N/A";
+    (document.getElementById('joiningDate') as HTMLElement).textContent = userData.joining_date.substring(0,10) || "N/A";
 }
 
 

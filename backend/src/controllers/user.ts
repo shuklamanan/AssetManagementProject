6ed12a37b2a75daa933af822aaee59c6a72217ef
shuklamanan <mananshukla1512@gmail.com>
2024-11-07 12:44:31 +0530

    Fix login user bug

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -7,16 +7,15 @@ import {
     ICreateUserQueryBody,
     ICreateUserRequestBody,
     ILoginUserRequestBody,
-    IMergeDetailsOfAssetAndUser,
     IUser
 } from "../interfaces/requestWithUser.ts";
 
 dotenv.config()
 
 async function hashPassword(password: string): Promise<string> {
-    const saltRounds: number = 10;
-    return await new Promise((resolve, reject): void => {
-        bcrypt.hash(password, saltRounds, function (err: Error | undefined, hash: string) {
+    const saltRounds : number = 10;
+    return await new Promise((resolve, reject) : void => {
+        bcrypt.hash(password, saltRounds, function (err :Error | undefined, hash : string) {
             if (err) reject(err)
             resolve(hash)
         });
@@ -32,8 +31,8 @@ const generateToken = (payload: JwtPayload) => {
     return jwt.sign(payload, secretKey, options);
 };
 
-function isValidPassword(password: string): boolean {
-    const regex: RegExp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,20}$/;
+function isValidPassword(password: string) : boolean {
+    const regex : RegExp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,20}$/;
     return regex.test(password);
 }
 
@@ -80,7 +79,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
             res.status(400).json({message: "some fields are missing in req"})
             return
         }
-        let response: IUser = await client.query("select * from users where username=$1", [username]);
+        let response : IUser = await client.query("select * from users where username=$1 and archived_at is null", [username]);
         if (response.rows.length === 0) {
             res.status(404).json({message: "user not found"})
             return
@@ -90,7 +89,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
             res.status(401).json({message: "user auth failed incorrect username or password"})
             return
         }
-        let payload: JwtPayload = {id: user.id}
+        let payload : JwtPayload = { id: user.id }
         let token: string = generateToken(payload)
         res.status(200).json({token: token});
         return
@@ -111,8 +110,8 @@ export const getRoles = async (req: Request, res: Response): Promise<void> => {
 
 export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
     try {
-        const response: IUser = await client.query("select username,first_name,last_name,role,email,phone_number,department,date_of_birth from users WHERE archived_at IS NULL ORDER BY role")
-        const allUsers: ICreateUserRequestBody[] = response?.rows;
+        const response: IUser = await client.query("SELECT username,first_name,last_name,role,email,phone_number,department,date_of_birth FROM users WHERE archived_at IS NULL ORDER BY role")
+        const allUsers : ICreateUserRequestBody[] = response?.rows;
         res.status(200).json(allUsers ?? []);
         return;
     } catch (error: any) {
@@ -126,14 +125,14 @@ export const getProfileDetails = async (req: Request, res: Response): Promise<vo
         console.log(req.body.user);
         res.status(200).json({
             "username": req.body.user.username,
-            "firstName": req.body.user.first_name,
-            "lastName": req.body.user.last_name,
+            "first_name": req.body.user.first_name,
+            "last_name": req.body.user.last_name,
             "role": req.body.user.role,
             "email": req.body.user.email,
             "phone_number": req.body.user.phone_number,
             "department": req.body.user.department,
-            "dateOfBirth": req.body.user.date_of_birth,
-            "joiningDate": req.body.user.created_at,
+            "date_of_birth": req.body.user.date_of_birth,
+            "joining_date": req.body.user.created_at,
         });
         return;
     } catch (error: any) {

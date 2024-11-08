a326b02750ce48977d0a9885ce974c72b22cf273
shuklamanan <mananshukla1512@gmail.com>
2024-11-08 18:09:09 +0530

    Refactor code

---
+++ frontend/src/functions/api.ts
@@ -0,0 +1,25 @@
+const api: string = "http://localhost:5001/";
+const userApi:string = api + "users/";
+const assetApi:string =  api + "assets/";
+
+export const loginApi:string = userApi + "login/";
+export const signupApi:string = userApi + "signup/";
+
+export const getAllUsersApi:string = userApi;
+export const getProfileApi:string = userApi + "profile/";
+export const getRolesApi:string = userApi + "roles/";
+export const createUserViaAdminApi:string = userApi;
+export const deleteUserApi:string = userApi;
+
+export const getAllAssetsApi:string = assetApi;
+export const createAssetApi:string = assetApi;
+export const assetHistoryApi:string = assetApi + "history/";
+export const assetAssignApi:string = assetApi + "assign/";
+export const assetUnassignApi:string = assetApi + "unassign/";
+export const updateAssetApi:string = assetApi;
+export const deleteAssetApi:string = assetApi;
+
+export const headers :Record<string,string> = {
+    'Authorization': `${localStorage.getItem('token')}`,
+    'Content-Type': 'application/json'
+}

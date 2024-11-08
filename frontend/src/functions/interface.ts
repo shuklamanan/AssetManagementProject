fd7c7afcae9f57b0630afff6e1e6e76cc023db3d
shuklamanan <mananshukla1512@gmail.com>
2024-11-08 14:27:43 +0530

    Feat Add interfaces

--- frontend/src/functions/interface.ts
+++ frontend/src/functions/interface.ts
@@ -16,3 +16,27 @@ export interface IUser {
     date_of_birth: Date | string,
     created_at ?: Date | string,
 }
+
+export interface IAssetHistory {
+    user_id: string | null;
+    username: string | null;
+    asset_id: number;
+    asset_name: string;
+    assigned_at: string | null;
+    unassigned_at: string | null;
+}
+
+export interface IBodyStructureForAPI{
+    username : string,
+    password : string,
+}
+
+export interface IBodyStructureForUserAPI{
+    username : string,
+    firstName : string,
+    lastName : string,
+    email : string,
+    password : string,
+    phoneNumber : number,
+    dateOfBirth : string,
+}

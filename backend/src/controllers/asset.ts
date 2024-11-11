7e6443351ccb0a83fdc3c0b258948a597760e340
utsav-mehta-noovosoft <utsav.mehta@noovosoft.com>
2024-11-11 17:35:36 +0530

    Refactor asset & assetRequest

--- backend/src/controllers/asset.ts
+++ backend/src/controllers/asset.ts
@@ -1,9 +1,12 @@
 import client from "../../postgresConfig.ts"
 import {Request, Response} from "express";
 import {
-    IAsset, IAssetDeleteOrNot, ICreateAssetQueryBody, ICreateAssetRequestBody, IDecideHolderOfAsset,
-    IMergeDetailsOfAssetAndUser, IMergeDetailsOfAssetAndUserAndAssetHistory,
-    IUserAndAsset, IUserAndAssetAndAssetHistory, IUserDeleteOrNot,
+    IAsset,
+    ICreateAssetQueryBody,
+    ICreateAssetRequestBody,
+    IDecideHolderOfAsset,
+    IMergeDetailsOfAssetAndUser,
+    IMergeDetailsOfAssetAndUserAndAssetHistory,
 } from "../interfaces.ts";
 import {Asset} from "../viewModels/assets.ts";
 import {AssetHistory} from "../viewModels/assetHistory.ts";
@@ -20,7 +23,7 @@ export const createAssets = async (req: Request, res: Response): Promise<void> =
             res.status(400).json({message: "some required fields are missing in req"})
             return;
         }
-        const response: IAsset = await client.query("insert into assets(name, asset_type, config,user_id) values ($1,$2,$3,$4) RETURNING *", [name, assetType, JSON.stringify(config), userId ?? null])
+        const response: IAsset = await client.query("insert into assets(name, asset_type, config,user_id) values ($1,$2,$3,$4)", [name, assetType, JSON.stringify(config), userId ?? null])
         if (userId) {
             await client.query("INSERT INTO asset_history (asset_id,user_id,assigned_by,assigned_at) VALUES ($1,$2,$3,now())", [response?.rows[0].id, userId, req.body.user.id]);
         }
@@ -34,25 +37,24 @@ export const createAssets = async (req: Request, res: Response): Promise<void> =
 
 export const getAllAssets = async (req: Request, res: Response): Promise<void> => {
     try {
-        let response : IMergeDetailsOfAssetAndUser[];
-            response = (await client.query(`SELECT 
-                                                  a.id,
-                                                  a.name,
-                                                  a.asset_type,
-                                                  a.user_id,
-                                                  a.config,
-                                                  u.username,
-                                                  u.first_name,
-                                                  u.last_name,
-                                                  u.email,
-                                                  u.phone_number
-                                           FROM assets a
-                                                    LEFT JOIN users u ON u.id = a.user_id
-                                           WHERE a.archived_at IS NULL
-                                             AND u.archived_at IS NULL
-                                          ORDER BY a.id
-            `)).rows;
-        const allAssets  = response.map(asset=>new Asset(asset));
+        let response: IMergeDetailsOfAssetAndUser[];
+        response = (await client.query(`SELECT a.id,
+                                               a.name,
+                                               a.asset_type,
+                                               a.user_id,
+                                               a.config,
+                                               u.username,
+                                               u.first_name,
+                                               u.last_name,
+                                               u.email,
+                                               u.phone_number
+                                        FROM assets a
+                                                 LEFT JOIN users u ON u.id = a.user_id
+                                        WHERE a.archived_at IS NULL
+                                          AND u.archived_at IS NULL
+                                        ORDER BY a.id
+        `)).rows;
+        const allAssets = response.map(asset => new Asset(asset));
         res.status(200).json(allAssets ?? []);
         return;
     } catch (error: any) {
@@ -85,20 +87,18 @@ export const assetAssign = async (req: Request, res: Response): Promise<void> =>
             res.status(400).send({error: "Please enter all required fields"});
             return;
         }
-        const assetDeletedOrNot : number = (await client.query("SELECT 1 AS asset_deleted_or_not FROM assets WHERE id=$1 AND archived_at IS NOT NULL", [req.body.assetId])).rows.length;
-        const userDeletedOrNot :number = (await client.query("SELECT 1 AS user_deleted_or_not FROM users WHERE id=$1 AND archived_at IS NOT NULL", [req.body.userId])).rows.length;
-        if(assetDeletedOrNot) {
+        const assetDeletedOrNot: number = (await client.query("SELECT 1 AS asset_deleted_or_not FROM assets WHERE id=$1 AND archived_at IS NOT NULL", [req.body.assetId])).rows.length;
+        const userDeletedOrNot: number = (await client.query("SELECT 1 AS user_deleted_or_not FROM users WHERE id=$1 AND archived_at IS NOT NULL", [req.body.userId])).rows.length;
+        if (assetDeletedOrNot) {
             res.status(400).json({"message": "You can not assign asset because This asset is deleted"});
             return;
         }
-        if(userDeletedOrNot){
+        if (userDeletedOrNot) {
             res.status(400).json({"message": "You can not assign asset to this user because This user is deleted"});
             return;
         }
-        const response: IDecideHolderOfAsset = await client.query("SELECT 1 AS asset_holder_or_not FROM assets WHERE id=$1 AND user_id IS NOT NULL", [req.body.assetId])
-        console.log(req.body);
-        console.log(response?.rows)
-        if (!response?.rows[0]?.asset_holder_or_not) {
+        const response: number = (await client.query("SELECT 1 AS asset_holder_or_not FROM assets WHERE id=$1 AND user_id IS NOT NULL", [req.body.assetId])).rows.length
+        if (response) {
             await client.query("UPDATE assets SET user_id = $1 WHERE id = $2", [req.body.userId, req.body.assetId]);
             await client.query("INSERT INTO asset_history (asset_id,user_id,assigned_by,assigned_at) VALUES ($1,$2,$3,now())", [req.body.assetId, req.body.userId, req.body.user.id]);
             res.status(201).json({"message": "successfully assigning user"});
@@ -134,8 +134,8 @@ export const assetUnassign = async (req: Request, res: Response): Promise<void> 
 }
 export const getAssetHistory = async (req: Request, res: Response): Promise<void> => {
     try {
-        const assetsHistory : IMergeDetailsOfAssetAndUserAndAssetHistory[] = (await client.query("SELECT ah.user_id, u.username,a.id as asset_id,a.name as asset_name,ah.assigned_at,ah.unassigned_at FROM asset_history ah LEFT JOIN users u  ON u.id = ah.user_id JOIN assets a ON ah.asset_id = a.id  ORDER BY ah.id")).rows
-        const viewAssetsHistory = assetsHistory.map((assetHistory)=>new AssetHistory(assetHistory))
+        const assetsHistory: IMergeDetailsOfAssetAndUserAndAssetHistory[] = (await client.query("SELECT ah.user_id, u.username,a.id as asset_id,a.name as asset_name,ah.assigned_at,ah.unassigned_at FROM asset_history ah LEFT JOIN users u  ON u.id = ah.user_id JOIN assets a ON ah.asset_id = a.id  ORDER BY ah.id")).rows
+        const viewAssetsHistory = assetsHistory.map((assetHistory) => new AssetHistory(assetHistory))
         res.status(200).json(viewAssetsHistory);
         return;
     } catch (error: any) {
@@ -144,14 +144,14 @@ export const getAssetHistory = async (req: Request, res: Response): Promise<void
     }
 }
 
-export const deleteAsset = async (req:Request, res:Response):Promise<void> => {
+export const deleteAsset = async (req: Request, res: Response): Promise<void> => {
     try {
-        let assetId:string = req.params.id
-        await client.query("update asset_history set unassigned_at = $1 where asset_id = $2 and unassigned_at is null",[new Date(),assetId])
-        await client.query("update assets set user_id=null , archived_at = $1 where id = $2",[new Date(),assetId])
-        res.status(200).json({message:"asset deleted successfully"})
+        let assetId: string = req.params.id
+        await client.query("update asset_history set unassigned_at = $1 where asset_id = $2 and unassigned_at is null", [new Date(), assetId])
+        await client.query("update assets set user_id=null , archived_at = $1 where id = $2", [new Date(), assetId])
+        res.status(200).json({message: "asset deleted successfully"})
         return
-    } catch (error:any) {
+    } catch (error: any) {
         res.status(500).json({message: error?.message});
         return
     }

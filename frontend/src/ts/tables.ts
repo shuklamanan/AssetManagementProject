8f4664f718ac6502a811491bf5bad5be9a025fc9
shuklamanan <mananshukla1512@gmail.com>
2024-11-14 17:06:10 +0530

    Feat add dynamically generate table headers

--- frontend/src/ts/tables.ts
+++ frontend/src/ts/tables.ts
@@ -12,8 +12,45 @@ function isKeyValidOrNot(key:string):boolean{
     return key!='id' && key!='createdAt' && key!='joiningDate' && key!='userId' && key!='assetId';
 }
 
-export function createTable(parentNode:HTMLElement,data: (IUser[] | IAssetHistory[] | IAssetRequest[]),buttonFunctions:Function[],buttonAppear:boolean = false,buttonText:string[] = [],buttonClasses:string[]=[]):void{
+function convertCamelCaseToPascalCase(columnName:string):string{
+    let pascalCase:string = columnName[0].toUpperCase();
+    for(let i:number=1;i<columnName.length;i++){
+        if(columnName[i]>='A' && columnName[i]<='Z'){
+            pascalCase+=' ';
+            pascalCase+=columnName[i].toLowerCase();
+        }
+        else{
+            pascalCase+=columnName[i].toLowerCase();
+        }
+    }
+    return pascalCase;
+}
+
+function generateTableHeader(tableHead:HTMLElement,data:(IUser[] | IAssetHistory[] | IAssetRequest[]),buttonAppear:boolean=false,buttonText:string[]=[]){
+    const tableRow:HTMLTableRowElement = document.createElement("tr");
+    Object.entries((data[0])).forEach(([key, value]:[string,any]) => {
+        if(isKeyValidOrNot(key)){
+            const th:HTMLTableCellElement = document.createElement("th");
+            th.setAttribute('scope','col');
+            th.textContent = convertCamelCaseToPascalCase(key);
+            tableRow.appendChild(th);
+        }
+    })
+    if(buttonAppear){
+        for(let i:number=0;i<buttonText.length;i++) {
+            const th: HTMLTableCellElement = document.createElement("th");
+            th.setAttribute('scope', 'col');
+            th.textContent = buttonText[i];
+            tableRow.appendChild(th);
+        }
+    }
+    tableHead.appendChild(tableRow);
+}
+
+export function createTable(tableHead:HTMLElement,parentNode:HTMLElement,data: (IUser[] | IAssetHistory[] | IAssetRequest[]),buttonFunctions:Function[],buttonAppear:boolean = false,buttonText:string[] = [],buttonClasses:string[]=[]):void{
     parentNode.innerHTML = "";
+    if(!data.length){ return; }
+    generateTableHeader(tableHead,data,buttonAppear,buttonText);
     data.forEach((item):void => {
         const row : HTMLTableRowElement = document.createElement("tr");
         Object.entries(item).forEach(([key,value]:[string,any]) => {
@@ -32,7 +69,7 @@ export function createTable(parentNode:HTMLElement,data: (IUser[] | IAssetHistor
             for(let i:number=0;i<buttonFunctions.length; i++){
                 const deleteButtonCell : HTMLTableCellElement = document.createElement('td');
                 const deleteButton: HTMLButtonElement = createButtons(document.createElement('button'),"",buttonClasses[i],buttonText[i]);
-                deleteButton.onclick = () => buttonFunctions[i](item.id);
+                deleteButton.onclick = () => buttonFunctions[i](item);
                 deleteButtonCell.appendChild(deleteButton);
                 row.appendChild(deleteButtonCell);
             }

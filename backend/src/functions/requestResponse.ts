7160ee1b00909ebe0110d88d1c325d63dce60c9e
shuklamanan <mananshukla1512@gmail.com>
2024-11-18 18:25:25 +0530

    Remove Try & Catch

---
+++ backend/src/functions/requestResponse.ts
@@ -0,0 +1,27 @@
+import {Response} from "express";
+import client from "../../postgresConfig.ts"
+
+export const handleError = (res:Response,statusCode:number,message:string):void=>{
+    res.status(statusCode).json({message:message})
+    return;
+}
+
+export const handleSuccess = (res:Response,statusCode:number,message?:string,data?:any):void=>{
+    if(message){
+        res.status(statusCode).json({message:message})
+    }
+    else{
+        res.status(statusCode).json(data);
+    }
+    return;
+}
+
+export const executeQuery = async (query:string,params:any[],res:Response):Promise<any> => {
+    try{
+        return await client.query(query, params);
+    }
+    catch(err:any){
+        handleError(res,500,`Internal Server Error:${err.message}`);
+        return;
+    }
+}
